de97e8fc69a752ff8551bce28cc3d7e2857653de
Shantanu <joshishantanu4@gmail.com>
2016-03-08 14:12:47 -0800

    copy: update copy

--- copy.sh
+++ copy.sh
@@ -2,11 +2,15 @@
 cd $1 
 cp ~/.vimrc ./.vimrc 
 cp ~/.zshrc ./.zshrc
+cp ~/.gitconfig ./.gitconfig
 
 git add ./.vimrc 
 git commit -m "vimrc: update"
 
 git add ./.zshrc
 git commit -m "zshrc: update"
+
+git add ./.gitconfig
+git commit -m "gitconfig: update"
 git push origin master 
 
