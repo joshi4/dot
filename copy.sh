d3de3cd0ebcd1a8d188aa26f64b530d281ef1356
Shantanu <joshishantanu4@gmail.com>
2016-03-08 13:51:05 -0800

    copy:include zshrc

--- copy.sh
+++ copy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash 
 cd $1 
 cp ~/.vimrc ./.vimrc 
+cp ~/.zshrc ./.zshrc
 
 git add ./.vimrc 
 git commit -m "vimrc: update"
+
+git add ./.zshrc
+git commit -m "zshrc: update"
 git push origin master 
 
