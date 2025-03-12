3b9b13c8c8360aada5727f13778e5ef37ff6407c
Shantanu <joshishantanu4@gmail.com>
2025-03-12 15:17:05 -0700

    copy custom zsh

--- copy.sh
+++ copy.sh
@@ -5,6 +5,9 @@ cp ~/.gitconfig ./.gitconfig
 cp ~/.tmux.conf ./.tmux.conf
 cp ~/.config/shell/common.sh ./common.sh
 
+cp ~/.oh-my-zsh/custom/aliases.sh ./.oh-my-zsh/custom/aliases.sh
+cp ~/.oh-my-zsh/custom/customization.sh ./.oh-my-zsh/custom/customization.sh
+
 git add ./.vimrc
 git commit -m "vimrc: update"
 
@@ -17,6 +20,9 @@ git commit -m "gitconfig: update"
 
 git add ./.tmux.conf
 git commit -m "tmux: update"
+
+git add ./.oh-my-zsh/custom
+git commit -m "oh-my-zsh: update"
 
 git push -f origin master
 
