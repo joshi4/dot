a4bdd10c98329f1cfac8f3b300361b0f6e7c76b9
Shantanu <joshishantanu4@gmail.com>
2025-03-12 15:42:37 -0700

    update copy

--- copy.sh
+++ copy.sh
@@ -8,7 +8,7 @@ cp ~/.config/shell/common.sh ./common.sh
 cp ~/.oh-my-zsh/custom/aliases.sh ./.oh-my-zsh/custom/aliases.sh
 cp ~/.oh-my-zsh/custom/customization.sh ./.oh-my-zsh/custom/customization.sh
 
-cp ~/.config/ghostty/config.json ./.config/ghostty/config.json
+cp ~/.config/ghostty/config ./.config/ghostty/config
 
 git add ./.vimrc
 git commit -m "vimrc: update"
@@ -26,7 +26,7 @@ git commit -m "tmux: update"
 git add ./.oh-my-zsh/custom
 git commit -m "oh-my-zsh: update"
 
-git add ./.config/ghostty/config.json
+git add ./.config/ghostty/config
 git commit -m "ghostty: update"
 
 git push -f origin master
