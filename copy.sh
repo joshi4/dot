ab02c70156adf4d33f89d424bcd5d39cdb07f571
Shantanu <joshishantanu4@gmail.com>
2025-03-12 15:06:12 -0700

    get common.sh

--- copy.sh
+++ copy.sh
@@ -3,11 +3,13 @@ cp ~/.vimrc ./.vimrc
 cp ~/.zshrc ./.zshrc
 cp ~/.gitconfig ./.gitconfig
 cp ~/.tmux.conf ./.tmux.conf
+cp ~/.config/shell/common.sh ./common.sh
 
 git add ./.vimrc
 git commit -m "vimrc: update"
 
 git add ./.zshrc
+git add ./common.sh
 git commit -m "zshrc: update"
 
 git add ./.gitconfig
